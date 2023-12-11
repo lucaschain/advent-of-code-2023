089b6cc64b0c7c3beb76dc70abbdececd24c0d3e
Lucas Chain <sou@lucaschain.com.br>
2023-12-11 17:35:24 -0300

    embeded pprof server

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/lucaschain/advent-of-code/days"
 )
 
 func main() {
+	go func() {
+		http.ListenAndServe("localhost:8080", nil)
+	}()
 	exercises := []func() string{
 		days.Day1,
 		days.Day2,
